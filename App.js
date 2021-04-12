48fd47cfc982e844ac673c6a98a020b139bf93aa
Yingjian Pei <yjpei@ucsd.edu>
2021-04-11 17:41:35 -0700

    use effect

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, SafeAreaView} from 'react-native';
 import CourseList from './components/CourseList';
 import Course from './components/Course';
@@ -29,22 +29,37 @@ const schedule = {
   ]
 };
 
-const getCourseNumber = course => (
-  course.id.slice(1)
-)
+// const getCourseNumber = course => (
+//   course.id.slice(1)
+// )
 
 const Banner = ({title}) => (
-  <Text style= {styles.bannerStyle}>{title}</Text>
-)
+  <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
+);
 
 const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule =  async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+  
   return (
     <SafeAreaView style={styles.container}>
       <Banner title={schedule.title} />
       <CourseList courses={schedule.courses} />
     </SafeAreaView>
-  )
-}
+  );
+};
+
 
 const styles = StyleSheet.create({
   container: {
